cfad4c2b8ab0398408ac64ff6121666774dd3f25
James Estill <JamesEstill@gmail.com>
2012-04-12 23:47:56 -0400

    Updated tr_index_species_tree.pl -- Added simplified BSD license

--- scripts/tr_index_species_tree.pl
+++ scripts/tr_index_species_tree.pl
@@ -7,7 +7,7 @@
 #                                                           |
 # CONTACT: JamesEstill_at_gmail.com                         |
 # STARTED: 11/30/2010                                       |
-# UPDATED: 11/30/2010                                       |
+# UPDATED: 04/12/2012                                       |
 #                                                           |
 # DESCRIPTION:                                              | 
 #  Index species trees in the tree reconciliaiotn database. |
@@ -777,33 +777,53 @@ copying branches.
 
 =head1 LICENSE
 
-This file is part of BioSQL.
 
-BioSQL is free software: you can redistribute it and/or modify it
-under the terms of the GNU Lesser General Public License as
-published by the Free Software Foundation, either version 3 of the
-License, or (at your option) any later version.
+Copyright (c) 2012, The Arizona Board of Regents on behalf of 
+The University of Arizona.
 
-BioSQL is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU Lesser General Public License for more details.
+All rights reserved.
 
-You should have received a copy of the GNU Lesser General Public License
-along with BioSQL. If not, see <http://www.gnu.org/licenses/>.
+Developed by: iPlant Collaborative as a collaboration between
+participants at BIO5 at The University of Arizona (the primary hosting
+institution), Cold Spring Harbor Laboratory, The University of Texas at
+Austin, and individual contributors. Find out more at 
+http://www.iplantcollaborative.org/.
+
+Redistribution and use in source and binary forms, with or without 
+modification, are permitted provided that the following conditions are
+met:
+
+ * Redistributions of source code must retain the above copyright 
+   notice, this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright 
+   notice, this list of conditions and the following disclaimer in the 
+   documentation and/or other materials provided with the distribution.
+ * Neither the name of the iPlant Collaborative, BIO5, The University 
+   of Arizona, Cold Spring Harbor Laboratory, The University of Texas at 
+   Austin, nor the names of other contributors may be used to endorse or 
+   promote products derived from this software without specific prior 
+   written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED 
+TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A 
+PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT 
+HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS 
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 =head1 AUTHORS
 
 James C. Estill E<lt>JamesEstill at gmail.comE<gt>
 
-Hilmar Lapp E<lt>hlapp at gmx.netE<gt>
-
-William Piel E<lt>william.piel at yale.eduE<gt>
-
 =head1 HISTORY
 
-Started: 07/04/2007
+Started: 11/30/2010
 
-Updated: 08/19/2007
+Updated: 04/12/2012
 
 =cut
